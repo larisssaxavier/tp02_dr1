0d7d049261a144c8ab8321aeeec7c06a0f674b78
larisssa <larissa.xaraujo@al.infnet.edu.br>
2025-03-14 22:23:36 -0300

    calculadora de descontos

--- src/calculadoraDescontos.java
+++ src/calculadoraDescontos.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class calculadoraDescontos {
-    public static double calcularDescontos(double valorCompra){
+    public static String calcularDescontos(double valorCompra){
         double desconto;
         double valorFinal;
+        String porcentagem;
         if(valorCompra > 1000){
+            porcentagem = "10%";
             desconto = valorCompra * 0.1;
             valorFinal = valorCompra - desconto;
         }
         else if(valorCompra > 500 && valorCompra < 1000){
+            porcentagem = "5%";
             desconto = valorCompra * 0.05;
             valorFinal = valorCompra - desconto;
         }
         else{
+            porcentagem = "0%";
             desconto = 0;
             valorFinal = valorCompra - desconto;
         }
-        return valorFinal;
+        return " Desconto aplicado: " + porcentagem + "\n Valor final: " + valorFinal;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -24,8 +28,8 @@ public class calculadoraDescontos {
         System.out.print("Digite o valor total da compra: R$");
         double valorCompra = input.nextDouble();
 
-        double descontoTotal = calcularDescontos(valorCompra);
+        String descontoTotal = calcularDescontos(valorCompra);
 
-        System.out.println("Valor final com desconto: R$" + descontoTotal);
+        System.out.println (descontoTotal);
     }
 }
